e848c4b5f38b67a99ec6a3f012755ec4c74232c7
ahkong <ahreum0412@naver.com>
2022-01-30 12:00:13 +0900

    init

---
+++ src/main/kotlin/com/example/springboot_with_kotlin/SpringbootWithKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springboot_with_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootWithKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootWithKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springboot_with_kotlin/SpringbootWithKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springboot_with_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootWithKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
